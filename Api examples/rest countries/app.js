26bd3da80cd1fa054f5b87644f6078134748e30d
Tusar <tusarahmed141@gmail.com>
2022-02-26 08:50:36 +0600

    Dynamic country load

--- Api examples/rest countries/app.js
+++ Api examples/rest countries/app.js
@@ -1,33 +1,66 @@
+// const loadCountries = () => {
+//   fetch("https://restcountries.com/v3.1/all")
+//     .then(response => response.json())
+//     .then(countries => displayCountries(countries))
+// };
+
+// loadCountries()
+
+// const countryContainer = document.getElementById('country-container');
+
+// const displayCountries = dataOfData => {
+//     dataOfData.forEach(data => {
+//         console.log(data);
+//         const div = document.createElement('div');
+//         div.classList.add('col-md-3')
+
+//         const flag = document.createElement('img');
+//         flag.classList.add('img-fluid')
+//         flag.src = data.flags.png;
+//         div.appendChild(flag)
+
+//         const h3 = document.createElement('h3');
+//         h3.innerText = data.name.official;
+//         div.appendChild(h3);
+
+//         const h5 = document.createElement('h5');
+//         h5.innerText = `-- ${data.capital}`;
+//         div.appendChild(h5);
+
+//         countryContainer.appendChild(div)
+//     });
+// }
+
+// Another Example
 const loadCountries = () => {
   fetch("https://restcountries.com/v3.1/all")
-    .then(response => response.json())
-    .then(countries => displayCountries(countries))
+    .then((response) => response.json())
+    .then((countries) => displayCountries(countries));
 };
 
-loadCountries()
+loadCountries();
 
-const countryContainer = document.getElementById('country-container');
+const countryContainer = document.getElementById("country-container");
 
-const displayCountries = dataOfData => {
-    dataOfData.forEach(data => {
-        console.log(data);
-        const div = document.createElement('div');
-        div.classList.add('col-md-3')
+const displayCountries = (data) => {
+  data.forEach((countries) => {
+    const div = document.createElement("div");
+    div.classList.add("country");
 
-        const flag = document.createElement('img');
-        flag.classList.add('img-fluid')
-        flag.src = data.flags.png;
-        div.appendChild(flag)
+    const flag = document.createElement('img');
+    flag.src = countries.flags.png;
+    flag.style.borderRadius = '.4rem'
+    div.appendChild(flag);
+    // console.log(countries.flags.png);
 
-        const h3 = document.createElement('h3');
-        h3.innerText = data.name.official; 
-        div.appendChild(h3);
+    const h3 = document.createElement("h3");
+    h3.innerText = countries.name.common;
+    div.appendChild(h3);
 
-        const h5 = document.createElement('h5');
-        h5.innerText = `-- ${data.capital}`;
-        div.appendChild(h5);
+    const p = document.createElement("p");
+    p.innerText = `- ${countries.capital}`;
+    div.appendChild(p);
 
-
-        countryContainer.appendChild(div)
-    });
-}
+    countryContainer.appendChild(div);
+  });
+};
